7e0454cb2da72c21950acea1404b0099901e6876
ejpark12 <ejpark@wisc.edu>
2021-10-28 10:01:39 -0500

    Revert "Added skeleton code for account page"
    This reverts commit f80b26bd0ff11f84a48fa29f132c9f3a68630de6.

--- react-node-app/capitolcarcleaners/src/App.js
+++ react-node-app/capitolcarcleaners/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import logo from './logo.svg';
-import CreateAccount from './CreateAccount'
 import './App.css';
 
 function App() {
@@ -19,7 +18,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <CreateAccount />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
   );
