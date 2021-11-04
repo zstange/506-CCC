63bbc7656c7fb4e3a47b903c77a42d9287d9c757
ejpark12 <ejpark@wisc.edu>
2021-11-03 23:35:17 -0500

    improved skeleton code
    appointment cards now show the user's information, modal window pop ups for handling modify and deletes appear

--- react-node-app/capitolcarcleaners/src/components/ViewAppointments.js
+++ react-node-app/capitolcarcleaners/src/components/ViewAppointments.js
@@ -1,62 +1,183 @@
-import React, {useState} from "react"; 
+import React, {useEffect,useState} from "react"; 
 import Axios from 'axios';
 import '../css/ViewAppointments.css';
-import { Card, ListGroup, ListGroupItem, Form, Button, Row, Col} from "react-bootstrap";
+import { Card, ListGroup, ListGroupItem, Form, Button, Row, Col, Modal} from "react-bootstrap";
 import {LinkContainer as Link} from 'react-router-bootstrap'
 import { Redirect } from 'react-router-dom';
 
-function GenerateAppointmentList() {
-    const [appointments, setAppointments] = useState([{aid: "1",a: "apple"}, {aid: "2", a: "bapple"}])
-    const GenerateList = appointments.map((appointment) =>
-    <>
-        <br></br>
-        <Card id = {appointment.aid} style={{width: '75rem' }}>
-        <Card.Header>service goes here</Card.Header>
-        <Card.Body>
-            <Card.Title>date and time goes here</Card.Title>
-            
-            <ListGroup className="list-group-flush">
-                <ListGroupItem>car goes here</ListGroupItem>
-                <ListGroupItem>additional info</ListGroupItem>
-            </ListGroup> 
-        </Card.Body>
-        <Button className="btn-sm" variant="primary">Modify Appointment</Button>
-        </Card>
-    </>
-    );
+function GenerateAppointmentList(props) {
+    // assume user id is 25 for example
+    let id = 25;
+    //dummy appointment table
 
-    if (appointments.length > 0)
-        return GenerateList
-    else {
-        return (
+    const [appointments, setAppointments] = useState([])
+    const [vehicles, setVehicles] = useState([])
+    const [modal, showModal] = useState(false)
+    const [modalMode, setModalMode] = useState("")
+    const [modalMessage, setModalMessage] = useState("")
+
+    useEffect(() => {
+        async function fetchTables() {
+            let apps = []
+            let modifys = []
+
+            await Axios.get("http://localhost:3001/getAppointmentTable",{
+            }).then((response) => {
+                if(response.data.err) {
+                    console.log(response.data.err)
+                }
+                else if (response.data.message) {
+                    console.log(response.data.err)
+                } 
+                else {     
+                    for (let i = 0; i < response.data.length; i++) {
+                        let newItem = {
+                            aid: response.data.data[i].aid,
+                            uid: response.data.data[i].uid,
+                            vid: response.data.data[i].vid,
+                            dateTime: response.data.data[i].dateTime,
+                            service: response.data.data[i].service,
+                            make: null,
+                            model: null,
+                            year: null,
+                            color: null,
+                            license: null,
+                            additionalInfo: response.data.data[i].additionalInfo,
+                            showModifyModal: false,
+                            showDeleteModal: false
+                        }
+                        let newModify = {show:false};
+                        apps.push(newItem)
+                        modifys.push(newModify)
+                    } 
+                    return apps    
+                }
+            });
+
+            let vehic = []
+        
+            await Axios.post("http://localhost:3001/getVehicles",{
+                uid: id
+            }).then((response) => {
+                if(response.data.err) {
+                    console.log(response.data.err)
+                }
+                else if (response.data.message) {
+                    console.log(response.data.err)
+                } 
+                else {     
+                    for (let i = 0; i < response.data.length; i++) {
+                        let newItem = {
+                            vid: response.data.data[i].vid,
+                            make: response.data.data[i].make,
+                            model: response.data.data[i].model,
+                            year: response.data.data[i].year,
+                            color: response.data.data[i].color,
+                            license: response.data.data[i].licensePlate
+                        }
+                        vehic.push(newItem)
+                    } 
+                    return vehic   
+                }
+            });
+
+            for (let i = 0; i < apps.length; i++) {
+                for (let j = 0; j < vehic.length; j++) {
+                    if (apps[i].vid === vehic[j].vid) {
+                        apps[i].make = vehic[j].make
+                        apps[i].model = vehic[j].model
+                        apps[i].year = vehic[j].year
+                        apps[i].color = vehic[j].color
+                        apps[i].license = vehic[j].license
+                    }
+                }
+                
+            }
+
+            setAppointments(apps)
+            setVehicles(vehic)
+        }
+        fetchTables()
+    }, [id]);
+
+    const handleCardClick = (event) => {
+        let mode = String(event.target.id).substring(0,6)
+        let aid = String(event.target.id).substring(7,String(event.target.id).length)
+        showModal(!modal)
+        setModalMode(mode.charAt(0).toUpperCase() + mode.slice(1) + " Appointment")
+
+        if (mode === "delete")
+            setModalMessage("Are you sure you want to delete this appointment?")
+    }
+
+    const handleModalClick = (event) => {
+        let mode = String(event.target.id).substring(0,6)
+        let aid = String(event.target.id).substring(7,String(event.target.id).length)
+        showModal(!modal)
+        setModalMessage("")
+    }
+
+    return (
+        <>
+        <Modal show={modal} centered id = "modal">
+        <Modal.Header >
+        <Modal.Title>{modalMode}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <p>{modalMessage}</p>
+                <div style={{textAlign: 'center'}}>
+                    <Button id="confirm" variant="primary" size='sm' style={{margin: '5px'}} onClick={handleModalClick}>Confirm</Button>
+                    <Button id="cancel" variant="secondary" size='sm' style={{margin: '5px'}} onClick={handleModalClick}>Cancel</Button>
+                </div>
+        </Modal.Body>                   
+        </Modal>
+        <div >
+        <GenerateList appointments = {appointments} />
+        </div>
+        </>
+    )
+
+    function GenerateList(props) {
+        let appointments = props.appointments
+        const GenerateList = appointments.map((appointment, index) =>
             <>
-                <br></br>
-                <Card style={{width: '75rem' }}>
-                <Card.Header>No Upcoming Appointments</Card.Header>
-                <Card.Body>
-                    <Card.Title></Card.Title>
-                </Card.Body>
-                </Card>
+            <br></br>
+            <Card id = {"card_"+appointment.aid} >
+            <Card.Header>{appointment.dateTime}</Card.Header>
+            <Card.Body>
+                <Card.Title>{appointment.service}</Card.Title>
+                
+                <ListGroup className="list-group-flush">
+                    <ListGroupItem>{appointment.make+"-"+appointment.model+"-"+appointment.year+"-"+appointment.color+"-"+appointment.license}</ListGroupItem>
+                    <ListGroupItem>{appointment.additionalInfo}</ListGroupItem>
+                </ListGroup> 
+            </Card.Body>
+            <div style = {{textAlign: 'center', marginTop: '-10px', marginBottom: '15px'}}>
+            <Button id = {"modify-"+appointment.aid} style = {{marginLeft: '-5px'}} onClick={handleCardClick}>Modify</Button>   
+            <Button id = {"delete-"+appointment.aid} style = {{marginLeft: '5px'}} variant="danger" onClick={handleCardClick}>Delete</Button>
+            </div>
+            </Card>
             </>
         );
+    
+        return GenerateList
     }
+
 }
 
-class ViewAppointments extends React.Component {
+
+
+class ViewAppointments extends React.Component { 
+
   render() {
     return (
         <>
-        <br></br>
-        <div>
-            <h1 className="createAccountHeaders">View Appointments</h1>
-        </div>
         <Row style={{padding: '1%'}}>
             <div className="List">     
-                <h4 className="createAccountLabels mb-1">Upcoming Appointments:</h4>
                 <GenerateAppointmentList/>
                 <br></br>   
                 <Link to="/Cre">
-                    <Button className="btn-lg" style={{display: 'inline-block'}}>Schedule Appointment</Button>
+                    <Button className="btn" style={{display: 'inline-block'}}>Schedule Appointment</Button>
                 </Link>
             </div>     
         </Row> 
--- react-node-app/server/controllers/customer.js
+++ react-node-app/server/controllers/customer.js
@@ -1,16 +1,15 @@
-const { db } = require('../db.js')
+const  db  = require('../db.js')
 const { bcrypt, saltRounds} = require('../hash.js')
 
 const customerController ={
 createAcc(req, res) {
     const firstName = req.body.firstName
     const lastName = req.body.lastName
-    const email = "email@gmail.com"
-    const password = "password"
+    const email = req.body.email
+    const password = req.body.password
     const phoneNumber = req.body.phoneNumber
     const role = "user"
     const promotions = req.body.promotions
-    const role = "user"
   
    bcrypt.hash(password,saltRounds, (err, hash) =>{
     if(err){
@@ -60,6 +59,65 @@ addAppointment(req, res){
         });
 },
 
+
+getAppointment(req, res){
+    const sqlInsert = 
+    "SELECT * FROM appointmenttable"
+    db.query(sqlInsert
+        , (err, result) => {
+            if(err){
+                res.send({err: err});
+              }
+              else if (result != ""){
+                var appointment = JSON.parse(JSON.stringify(result));
+				var appointmentInfo = { appointmentID: appointment[0].aid, userID: appointment[0].uid, vehicleID: appointment[0].vid,
+				dateTime: appointment[0].dateTime, service: appointment[0].service, additionalInfo: appointment[0].additionalInfo, status: appointment[0].status, };
+				return res.json(appointmentInfo);
+              }
+              else{
+                res.send({message: "cannot get appointment information"})
+              }
+        });
+},
+
+getAppointmentTable(req, res){
+  const sqlInsert = 
+  "SELECT * FROM appointmenttable"
+  db.query(sqlInsert
+      , (err, result) => {
+          if(err){
+              res.send({err: err});
+            }
+            else if (result != ""){
+              
+              return res.json({data: JSON.parse(JSON.stringify(result)), length: result.length});
+            }
+            else{
+              res.send({message: "cannot get appointment information"})
+            }
+      });
+},
+
+getVehicles(req, res){
+  const uid = req.body.uid
+
+  const sqlInsert = 
+  "SELECT vid, make, model, year, color, licensePlate FROM vehicletable WHERE uid = ?"
+  db.query(sqlInsert, [uid]
+      , (err, result) => {
+          if(err){
+              res.send({err: err});
+            }
+            else if (result != ""){
+              
+              return res.json({data: JSON.parse(JSON.stringify(result)), length: result.length});
+            }
+            else{
+              res.send({message: "cannot get vehicle information"})
+            }
+      });
+},
+
 forgotPassword(req, res){
   const password = req.body.password
   const email = req.body.email
@@ -96,6 +154,51 @@ checkEmail(req, res){
       return res.json({value: false});
     }
   });
+
+},
+editAppointment(req, res){
+  const aid = req.body.aid
+  const vid = req.body.vid
+  const dateTime = req.body.dateTime
+  const service = req.body.service
+  const additionalInfo = req.body.additionalInfo
+  const status = "Not Ready"
+  const sqlInsert = 
+"UPDATE appointmenttable SET vid = ?, dateTime = ?, service = ?, additionalInfo = ?, status = ? WHERE aid = ?;"
+db.query(sqlInsert, [vid, dateTime, service, additionalInfo, status, aid], (err, result) => {
+
+  if(err){
+    res.send({err: err});
+  }
+  else if (result != ""){
+    var redir = { redirect: "/viewAppointment" };
+    return res.json(redir);
+  }
+  else{
+    res.send({message: "Please fill out all information required."})
+  }
+
+});
+
+},
+deleteAppointment(req, res){
+  const aid = req.body.aid
+  const sqlInsert = 
+"DELETE FROM appointmenttable WHERE aid = ?;"
+db.query(sqlInsert, [aid], (err, result) => {
+
+  if(err){
+    res.send({err: err});
+  }
+  else if (result != ""){
+    var redir = { redirect: "/viewAppointment" };
+    return res.json(redir);
+  }
+  else{
+    res.send({message: "Appointement is not found!"})
+  }
+
+});
 
 }
 
