f750f55916d4842481990f4eef97b3bee9883bb7
Preetham <mukkarapreetham@gmail.com>
2021-11-14 11:28:53 -0600

    some mysql config file for testing, and updated tests so now will show code coverage

---
+++ jest-mysql-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    databaseOptions: {
+        host: 'us-cdbr-east-04.cleardb.com',
+        port: 3306,
+        user: 'b58458d7479138',
+        password: 'c10de02a',
+        database: 'heroku_0266765e85d0fa2'
+    },
+    createDatabase: true,
+    truncateDatabase: true
+  };
