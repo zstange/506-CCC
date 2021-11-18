cbf4d8e8992aa4e3890188a0f1f6805ce65e5601
Zach Stange <zstange@wisc.edu>
2021-11-17 22:21:30 -0600

    redux-persist intialization

--- react-node-app/capitolcarcleaners/src/store.js
+++ react-node-app/capitolcarcleaners/src/store.js
@@ -1,12 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import {combineReducers} from "redux"; 
+import storage from 'redux-persist/lib/storage'; //defaults to LocalStorage for web
 import statusReducer from './statusSlice';
 import userIdReducer from './userIdSlice';
 import roleReducer from './roleSlice';
 
-export default configureStore({
-  reducer: {
-    loggedIn: statusReducer,
-    userId: userIdReducer,
-    role: roleReducer,
-  },
+const reducers = combineReducers({
+  loggedIn: statusReducer,
+  userId: userIdReducer,
+  role: roleReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+export default () => {
+  let store = configureStore({
+    reducer: persistedReducer,
+  });
+  let persistor = persistStore(store);
+  return { store, persistor }
+}
